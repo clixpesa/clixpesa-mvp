70d03d384e411b30554aabc25bc1b3959735b9f9
rizamoyi <samwelmoyi@gmail.com>
2023-02-21 10:18:35 +0300

    Refactor root modules and Setup theme

--- packages/dapp/App.js
+++ packages/dapp/App.js
@@ -1,20 +1,20 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, Box, Text } from 'native-base';
+import { theme } from './theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NativeBaseProvider theme={theme}>
+      <Box
+        flex={1}
+        bg="primary.100"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Text fontSize="xl">Hello, world!</Text>
+      </Box>
       <StatusBar style="auto" />
-    </View>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
