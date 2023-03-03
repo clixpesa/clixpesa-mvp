033f65ec558f1346f4348f77ca84e05e3268ee2a
Kachi <kachdekan@gmail.com>
2023-03-03 12:01:48 +0300

    fix bottom tabs styling

--- packages/dapp/navigation/app.navigator.js
+++ packages/dapp/navigation/app.navigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-remix-icon';
+import { Box, Text, Avatar, Pressable, HStack } from 'native-base'
 
 import { HomeScreen } from '../features/essentials';
 import { SpacesNavigator } from './spaces.navigator';
@@ -10,21 +11,26 @@ import { AccountNavigator } from './account.navigator';
 const Tab = createBottomTabNavigator();
 
 const TAB_ICON = {
-  Home: 'home-3-fill',
-  Spaces: 'safe-2-fill',
-  Loans: 'hand-coin-fill',
-  Account: 'user-3-fill',
+  Home: ['home-3-fill', 'home-3-line'],
+  Spaces: ['safe-2-fill', 'safe-2-line'],
+  Loans: ['hand-coin-fill', 'hand-coin-line'],
+  Account: ['user-3-fill', 'user-3-line']
 };
 
 const screenOptions = ({ route }) => {
   const iconName = TAB_ICON[route.name];
   return {
-    tabBarIcon: ({ color, size }) => (
-      <Icon name={iconName} size={size} color={color} />
+    tabBarIcon: ({ focused, size }) => (
+      <Box bg={focused ? 'primary.200' : '#ffffff'} rounded="2xl" px="5" py="1" mt="1">
+        <Icon name={focused ? iconName[0] : iconName[1]} size={22} color="#0F766E" />
+      </Box>
     ),
-    tabBarActiveTintColor: '#2DD4BF',
-    tabBarInactiveTintColor: '#115E59',
-  };
+    tabBarLabel: () => (
+      <Text _light={{ color: 'primary.900' }} fontSize="2xs" mb="0.5">
+        {route.name}
+      </Text>
+    ),
+  }
 };
 export const AppNavigator = () => {
   return (
