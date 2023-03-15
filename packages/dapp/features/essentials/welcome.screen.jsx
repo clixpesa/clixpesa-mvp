615be6c4115b71441a944ea0546082e253656563
Kachi <kachdekan@gmail.com>
2023-03-15 13:53:19 +0300

    add welcome screen

--- packages/dapp/features/essentials/welcome.screen.jsx
+++ packages/dapp/features/essentials/welcome.screen.jsx
@@ -1,8 +1,30 @@
-import { Text, Box } from 'native-base';
+import { Box, Heading, VStack, Button, Spacer } from 'native-base';
 
 const WelcomeScreen = () => (
-  <Box flex={1} bg="primary.100" alignItems="center" justifyContent="center">
-    <Text fontSize="xl">Welcome Screen!</Text>
+  <Box flex={1} bg="white" alignItems="center" justifyContent="center">
+    <Box width="85%" top="30%">
+      <Heading textAlign="center" color="blueGray.800">
+        Step into the future of money with Clixpesa
+      </Heading>
+    </Box>
+    <Spacer maxH="70%" />
+    <VStack space={3}>
+      <Button
+        rounded="3xl"
+        minW="65%"
+        _text={{ color: 'primary.100', fontWeight: 'semibold', mb: '0.5' }}
+      >
+        Create New Account
+      </Button>
+      <Button
+        variant="subtle"
+        rounded="3xl"
+        minW="65%"
+        _text={{ color: 'primary.700', fontWeight: 'semibold', mb: '0.5' }}
+      >
+        Use Existing Account
+      </Button>
+    </VStack>
   </Box>
 );
 
