7b05429e94d9670402d5958f8648926c7b975c9c
rizamoyi <samwelmoyi@gmail.com>
2023-06-06 22:47:11 +0300

    rebase to main

--- packages/dapp/components/contact.component.jsx
+++ packages/dapp/components/contact.component.jsx
@@ -1,9 +1,15 @@
-import { Avatar, Text, VStack, HStack } from 'native-base';
+import { Avatar, Text, VStack, HStack, Icon } from 'native-base';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Contact = ({ initials, fullName, phoneNo }) => {
   return (
     <HStack bg="#fff" space={4} p={1} mb={1}>
-      <Avatar>{initials}</Avatar>
+      <Avatar>
+        {initials}
+        <Avatar.Badge>
+          <Icon as={MaterialCommunityIcons} name="check" size="3" color="muted.800" />
+        </Avatar.Badge>
+      </Avatar>
       <VStack>
         <Text>{fullName}</Text>
         <Text>{phoneNo}</Text>
