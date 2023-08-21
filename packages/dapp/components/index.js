4e03058cc7d78fdfbb29bff7ba86e9e3e0487361
rizamoyi <samwelmoyi@gmail.com>
2023-08-21 15:07:39 +0300

    fix package dependancies errors

--- packages/dapp/components/index.js
+++ packages/dapp/components/index.js
@@ -1,7 +1,6 @@
 export { default as TransactionItem } from './transaction.component';
 export { default as SectionHeader } from './section-header.component';
 export { default as FeatureHomeCard } from './feature-home-card.component';
-<<<<<<< HEAD
 export { default as RoscaFeatureCard } from './rosca-feature-card.component';
 export { default as SpacesFeatureItem } from './spaces-feature-item.component';
 export { default as LoansFeatureItem } from './loans-feature-item.component';
@@ -12,7 +11,3 @@ export { default as CodeInput } from './code-input.component';
 export { TokenIcon as TokenIcon } from './token-icons.component';
 export { default as ScheduleActSheet } from './schedule-act-sheet.component';
 export { default as SuccessModal } from './success-modal.component';
-=======
-export { default as HeaderIcon } from './header-icon.component';
-export { default as Contact } from './contact.component';
->>>>>>> dfec54e (add rosca spaces feature)
